898084a399e8945710394d7252c57fb0f86fa392
danielcampos <ddregalo@gmail.com>
2019-04-06 13:24:43 +0100

    Create artwork RESTDataSource

---
+++ start/server/src/datasources/artwork.js
@@ -0,0 +1,50 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class ArtworkAPI extends RESTDataSource {
+    constructor() {
+      super();
+      this.baseURL = 'https://api.spacexdata.com/v2/';
+    }
+
+    async getAllArtworks() {
+        const response = await this.get('launches');
+        return Array.isArray(response)
+          ? response.map(artwork => this.artworkReducer(artwork))
+          : [];
+    }
+
+    async getArtworkById() {
+        const response = await this.get('launches');
+        return Array.isArray(response)
+          ? response.map(artwork => this.artworkReducer(artwork))
+          : [];
+    }
+
+    getArtworksByIds({ artworkIds }) {
+        return Promise.all(
+          artworkIds.map(artworkId => this.getArtworkById({ artworkId })),
+        );
+      }
+
+    artworkReducer(artwork) {
+        return {
+          id: artwork.flight_number || 0,
+          artist: {
+            id: artwork.rocket.rocket_id,
+            firstName: artwork.mission_name,
+            lastName: artwork.rocket.rocket_name,
+            dateOfBirth: parseInt(Math.floor(Math.random() * 2000)),
+            style: ABSTRACT,
+            isAlive: true
+          },
+          title: artwork.launch_site.site_name,
+          year: parseInt(Math.floor(Math.random() * 2000)),
+          medium: artwork.rocket.rocket_type,
+          height: parseInt(Math.floor(Math.random() * 200)),
+          width: parseInt(Math.floor(Math.random() * 500)),
+          style: ABSTRACT
+        };
+      }
+}
+  
+module.exports = ArtworkAPI;

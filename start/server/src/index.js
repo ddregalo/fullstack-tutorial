1ba2bc1db413eb044e570978705d8125d419b63b
danielcampos <ddregalo@gmail.com>
2019-04-07 21:57:18 +0100

    Add authentication for users + buy-cancel artwork mutation resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -3,16 +3,30 @@ const { createStore } = require('./utils');
 const typeDefs = require('./schema.js');
 const resolvers = require('./resolvers');
 const store = createStore();
+
 const ArtworkAPI = require('./datasources/artwork.js');
 const UserAPI = require('./datasources/user.js');
-const server = new ApolloServer({ 
+
+const isEmail = require('isemail');
+const server = new ApolloServer({
+        context: async ({ req }) => {
+            const auth = (req.headers && req.headers.authorization) || '';
+            const email = Buffer.from(auth, 'base64').toString('ascii');
+            
+            if (!isEmail.validate(email)) return {user: null};
+
+            const users = await store.users.findOrCreate({ where: { email } });
+            const user = users && users[0] ? users[0] : null;
+
+            return { user: { ...user.dataValues } };
+        },
         typeDefs,
         resolvers,
         dataSources: () => ({
             artworkAPI: new ArtworkAPI(),
             userAPI: new UserAPI({ store }),
         }),
-        introspection: true 
+        introspection: true
     });
 
 server.listen().then(({ url }) => {
--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -24,6 +24,45 @@ module.exports = {
         me: async (_, __, { dataSources }) => 
             dataSources.userAPI.findOrCreateUser(),
     },
+    Mutation: {
+        login: async (_, { email }, { dataSources }) => {
+          const user = await dataSources.userAPI.findOrCreateUser({ email });
+          if (user) return Buffer.from(email).toString('base64');
+        },
+        buyArtworks: async (_, { artworkIds }, { dataSources }) => {
+            const results = await dataSources.userAPI.buyArtworks({ artworkIds });
+            const artworks = await dataSources.artworkAPI.getArtworksByIds({
+              artworkIds,
+            });
+        
+            return {
+              success: results && results.length === artworkIds.length,
+              message:
+                results.length === artworkIds.length
+                  ? 'Artworks purchased successfully!'
+                  : `The following artworks couldn't be purchased: ${artworkIds.filter(
+                      id => !results.includes(id),
+                    )}`,
+                    artworks,
+            };
+        },
+        cancelArtworkPurchase: async (_, { artworkId }, { dataSources }) => {
+            const result = dataSources.userAPI.cancelArtworkPurchase({ artworkId });
+        
+            if (!result)
+                return {
+                success: false,
+                message: 'Failed to cancel purchase.',
+                };
+        
+            const artwork = await dataSources.artworkAPI.getArtworkById({ artworkId });
+            return {
+                success: true,
+                message: 'Artwork purchase cancelled.',
+                artworks: [artwork],
+            };
+        },
+    },
     Artist: {
         isAlive: async (artist, _, { dataSources }) => {
             const artwork = dataSources.artworkAPI.getArtworkById({ artworkId: id });

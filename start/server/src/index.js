7e1cba926cfa6388fe85b55a4ff9adb02ff14781
danielcampos <ddregalo@gmail.com>
2019-04-07 10:26:16 +0100

    Add artwork and user api dataSources to apollo server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,6 +2,13 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema.js');
 const server = new ApolloServer({ typeDefs });
 
+const store = createStore();
+
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
+
+    dataSources: () => ({
+        ArtworkAPI: new ArtworkAPI(),
+        UserAPI: new UserAPI({ store }),
+    })
   });
